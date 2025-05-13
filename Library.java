f79caa294435faa376d79fe0733b9033d629ca56
YasinMirnia <ya3mirnia@gmail.com>
2025-05-14 02:33:30 +0330

    file added to git

---
+++ Library.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Member {
+    private String name;
+    private String lastName;
+    private int studentId;
+    private String faculty;
+    private String field;
+    private int entranceYear;
+    private int membershipDateDay;
+    private int membershipDateMonth;
+    private int membershipDateYear;
+
+    // Setter Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+    public void setField(String field) {
+        this.field = field;
+    }
+    public void setEntranceYear(int entranceYear) {
+        this.entranceYear = entranceYear;
+    }
+    public void setMembershipDateDay(int membershipDateDay) {
+        this.membershipDateDay = membershipDateDay;
+    }
+    public void setMembershipDateMonth(int membershipDateMonth) {
+        this.membershipDateMonth = membershipDateMonth;
+    }
+    public void setMembershipDateYear(int membershipDateYear) {
+        this.membershipDateYear = membershipDateYear;
+    }
+
+    // Getter Methods
+   public String getName() {
+        return name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public String getFaculty() {
+        return faculty;
+    }
+    public String getField() {
+        return field;
+    }
+    public int getEntranceYear() {
+        return entranceYear;
+    }
+    public int getMembershipDateDay() {
+        return membershipDateDay;
+    }
+    public int getMembershipDateMonth() {
+        return membershipDateMonth;
+    }
+    public int getMembershipDateYear() {
+        return membershipDateYear;
+    }
+
+    // Show information Method
+    public void displayMemberInfo(){
+        System.out.println("First Name:" + name);
+        System.out.println("Last Name:" + lastName);
+        System.out.println("Student Id:" + studentId);
+        System.out.println("Faculty:" + faculty);
+        System.out.println("Field:" + field);
+        System.out.println("Entrance Year:" + entranceYear);
+        System.out.println("Member Since:" + membershipDateDay + "/" + membershipDateMonth + "/" + membershipDateYear);
+    }
+}
