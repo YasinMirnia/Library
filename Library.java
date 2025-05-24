a1c1a8b994ad8f61c23c1c7ee6bf50c2db9be862
YasinMirnia <ya3mirnia@gmail.com>
2025-05-24 14:00:59 +0330

    edit member has been added

--- Library.java
+++ Library.java
@@ -9,6 +9,9 @@ public class Library {
         List <Member> members = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Saturn's Library!\n");
+
+        int selectedNumber;
+        do {
         System.out.println("Select a Number for Countinue:");
         System.out.println("1: Add Member");
         System.out.println("2: Edit Member's Information");
@@ -21,9 +24,6 @@ public class Library {
         System.out.println("9: Disply Book Information:");
         System.out.println("10: Disply Book Information:");
         System.out.println("0: Exit\n");
-
-        int selectedNumber;
-        do {
             selectedNumber = input.nextInt();
             if (selectedNumber == 1){
                 System.out.println("Adding a Member:");
@@ -64,8 +64,52 @@ public class Library {
                 System.out.println("\n");
 
                 members.add(new Member(fullName, studentId, faculty, major, entranceYear, membershipDateDay, membershipDateMonth, membershipDateYear));
-            } else if (selectedNumber == 2) {
+                System.out.println("Member has been Added Successfully");
 
+            } else if (selectedNumber == 2) {
+                System.out.println("You Are Editing Member's Information!");
+                System.out.println("Choose A Member Using Student ID:\n");
+                int selectMember = input.nextInt();
+                for(int i = 0; i < members.size(); i++) {
+                    Member searchMember = members.get(i);
+                    if (searchMember.getStudentId() == selectMember) {
+                        searchMember.displayMemberInfo();
+                        System.out.println("which Info You Want to Edit?");
+                        int selectEditNumber = input.nextInt();
+                        do {
+                            switch(selectEditNumber) {
+                                case 1:
+                                    System.out.println("Editing Member's Full Name:");
+                                    System.out.println("Enter New Name:");
+                                    searchMember.setFullName(input.nextLine());
+                                    System.out.println("Member's Full Name has been edited!");
+                                    break;
+                                case 2:
+                                    System.out.println("Editing Member's ID:");
+                                    System.out.println("Enter New ID:");
+                                    searchMember.setStudentId(input.nextInt());
+                                    System.out.println("Members' Id has been edited!");
+                                    break;
+                                case 3:
+                                    System.out.println("Editing Member's Faculty:");
+                                    System.out.println("Enter New Faculty:");
+                                    searchMember.setFaculty(input.nextLine());
+                                    System.out.println("Member's Faculty has been edited!");
+                                    break;
+                                case 4:
+                                    System.out.println("Editing Member's Major:");
+                                    System.out.println("Enter New Major:");
+                                    searchMember.setField(input.nextLine());
+                                    System.out.println("Members's Major has been edited!");
+                                    break;
+                                default:
+                                    System.out.println("You Got Back!\n");
+                                }
+                        } while (selectEditNumber < 1 || selectEditNumber > 4);
+                    } else {
+                        System.out.println("Member Not Found!\n");
+                    }
+                }
             }
         } while (selectedNumber != 0);
     }
