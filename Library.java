485d9b4f960973c448a6836e084e17c20276c475
YasinMirnia <ya3mirnia@gmail.com>
2025-05-24 14:25:36 +0330

    deleting member has been added

--- Library.java
+++ Library.java
@@ -17,12 +17,10 @@ public class Library {
         System.out.println("2: Edit Member's Information");
         System.out.println("3: Delete Member");
         System.out.println("4: Display Member Information:");
-        System.out.println("5: Display All Members:");
-        System.out.println("6: Add Book");
-        System.out.println("7: Edit Book's Information");
-        System.out.println("8: Delete Book");
-        System.out.println("9: Disply Book Information:");
-        System.out.println("10: Disply Book Information:");
+        System.out.println("5: Add Book");
+        System.out.println("6: Edit Book's Information");
+        System.out.println("7: Delete Book");
+        System.out.println("8: Disply Book Information:");
         System.out.println("0: Exit\n");
             selectedNumber = input.nextInt();
             if (selectedNumber == 1){
@@ -81,6 +79,7 @@ public class Library {
                                 case 1:
                                     System.out.println("Editing Member's Full Name:");
                                     System.out.println("Enter New Name:");
+                                    input.nextLine();
                                     searchMember.setFullName(input.nextLine());
                                     System.out.println("Member's Full Name has been edited!");
                                     break;
@@ -93,12 +92,14 @@ public class Library {
                                 case 3:
                                     System.out.println("Editing Member's Faculty:");
                                     System.out.println("Enter New Faculty:");
+                                    input.nextLine();
                                     searchMember.setFaculty(input.nextLine());
                                     System.out.println("Member's Faculty has been edited!");
                                     break;
                                 case 4:
                                     System.out.println("Editing Member's Major:");
                                     System.out.println("Enter New Major:");
+                                    input.nextLine();
                                     searchMember.setField(input.nextLine());
                                     System.out.println("Members's Major has been edited!");
                                     break;
@@ -106,6 +107,29 @@ public class Library {
                                     System.out.println("You Got Back!\n");
                                 }
                         } while (selectEditNumber < 1 || selectEditNumber > 4);
+                    } else {
+                        System.out.println("Member Not Found!\n");
+                    }
+                }
+            } else if (selectedNumber == 3) {
+                System.out.println("You Are Deleting a Member:");
+                System.out.println("Choose A Member Using Student ID:\n");
+                int selectMember = input.nextInt();
+                for (int i = 0; i < members.size(); i++){
+                    Member searchMember = members.get(i);
+                    if (searchMember.getStudentId() == selectMember) {
+                        searchMember.displayMemberInfo();
+                        System.out.println("Are U sure for deleting this Member? Only 'Yes' or 'No'!\n");
+                        input.nextLine();
+                        String answer = input.nextLine();
+                        if (answer.equals("Yes")) {
+                            members.remove(i);
+                            System.out.println("Member Has Been Deleted!\n");
+                        } else if (answer.equals("No")) {
+                            System.out.println("You are Back!\n");
+                        } else {
+                            System.out.println("Invalid Value!\n");
+                        }
                     } else {
                         System.out.println("Member Not Found!\n");
                     }
